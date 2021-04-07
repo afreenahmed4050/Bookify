a6584c6fe049898bdeccf868bcce5d818481689c
Afreen <mail.afreenahmed@gmail.com>
2021-04-07 17:16:02 +0530

    User auth added

---
+++ scripts/auth.js
@@ -0,0 +1,25 @@
+const register= document.querySelector('#register-form')
+register.addEventListener('submit', (e) => {
+
+    const email = register['reg-email'].value;
+    const password = register['reg-password'].value;
+
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred);
+        register.reset();
+    })
+})
+
+const loginForm = document.querySelector('#signin-form');
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const email = loginForm['sign-email'].value;
+    const password = loginForm['sign-password'].value;
+
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred);
+        window.location = "booking-form.html";
+    })
+
+})
